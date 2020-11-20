772292676d9140cac28c60029228180dc0b102ad
James <james.hansung.park@gmail.com>
2020-11-20 17:11:23 -0500

    added anagram4

--- anagrams.rb
+++ anagrams.rb
@@ -6,14 +6,47 @@ end
 # p anagram?("gizmo", "sally")    #=> false
 # p anagram?("elvis", "lives")    #=> true
 
-def anagram_two?(str1, str2)
-    split = str2.split('')
-    str1.each_char do |char|
-        i = split.index(char)
-        split.delete_at(i) unless i == nil
-    end 
-    split.empty?
+def anagram_two?(str1, str2)                #O(n^2)       
+    split = str2.split('')                  #O(n)
+    
+    str1.each_char do |char|                #O(n)         # O(n^2)
+        i = split.index(char)               #O(n)
+        split.delete_at(i) unless i == nil  #O(n)
+    end
+
+    split.empty?                            #O(1)
 end
 
-p anagram_two?("gizmo", "sally")    #=> false
-p anagram_two?("elvis", "lives")    #=> true
+# p anagram_two?("gizmo", "sally")    #=> false
+# p anagram_two?("elvis", "lives")    #=> true
+
+def anagram3(str1, str2)                    #O(nlogn)
+    split1 = str1.split("").sort            #O(n) + O(nlogn) = O(nlogn)
+    split2 = str2.split("").sort            #O(n) + O(nlogn) = O(nlogn)
+
+    split1 == split2                        #O(n)
+end
+
+# p anagram3("gizmo", "sally")    #=> false
+# p anagram3("elvis", "lives")    #=> true
+
+def anagram4(str1, str2)
+    count = Hash.new(0)         #O(1)
+    
+    str1.each_char do |char|    #O(n)
+        count[char] += 1        #O(1)
+    end
+
+    str2.each_char do |char|    #O(n)
+        count[char] -= 1        #O(1)
+    end
+
+    # count.values.uniq.length == 1
+    count.values.each do |v|    #O(n)
+        return false if v > 0
+    end
+    true
+end
+
+p anagram4("gizmo", "sally")    #=> false
+p anagram4("elvis", "lives")    #=> true
--- execution_time_difference.rb
+++ execution_time_difference.rb
@@ -1,12 +1,12 @@
 # Given a list of integers find the smallest number in the list.
 require 'byebug'
 def my_min(arr)         #O(n^2) 
-    min = 0 #O(1)
+    min = 0             #O(1)
 
-    (0...arr.length).each do |i|     #O(n)
-        min = arr[i] < min ? arr[i] : min    #O(1)
-        (i+1...arr.length).each do |j|    #O(n)
-            min = arr[j] < min ? arr[j] : min #O(1)
+    (0...arr.length).each do |i|                #O(n)
+        min = arr[i] < min ? arr[i] : min       #O(1)
+        (i+1...arr.length).each do |j|          #O(n)
+            min = arr[j] < min ? arr[j] : min   #O(1)
         end 
     end 
     min
@@ -15,7 +15,7 @@ end
 # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(list)  # =>  -5
 
-def my_mintwo(arr)  #O(n)
+def my_mintwo(arr)          #O(n)
     min = 0
 
     i = 0
@@ -46,28 +46,24 @@ p lcs(list)
 # write a new function using O(n) time with O(1) memory
 def lcs2(arr)    
     i = 0
-    j = 1
 
     large_sum = 0
     current_sum = arr[i]
 
     until i == arr.length - 1       # cs = 2
         # add to current sum the next value
-        current_sum += arr[j]       # cs 
+        current_sum += arr[i]       # cs 
         
+        if current_sum < 0
+            current_sum = 0
+        end
+
         # compare and replace large_num if current_sum is greater
         if large_sum < current_sum
             large_sum = current_sum
         end
 
-        # increment j to iterate through to the next value otherwise, increment i to move onto the next sum iteration
-        if j != arr.length - 1
-            j += 1
-        else
-            i += 1
-            current_sum = arr[i]
-            j = i + 1
-        end
+        i += 1
     end
 
     large_sum
