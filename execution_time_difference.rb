a9f95bde39cd84292ee297c9025a6f18e1547041
James <james.hansung.park@gmail.com>
2020-11-20 15:30:57 -0500

    updated largest contiguous sub-sum methods

--- execution_time_difference.rb
+++ execution_time_difference.rb
@@ -42,3 +42,36 @@ end
 
 list = [2, 3, -6, 7, -6, 7]
 p lcs(list)
+
+# write a new function using O(n) time with O(1) memory
+def lcs2(arr)    
+    i = 0
+    j = 1
+
+    large_sum = 0
+    current_sum = arr[i]
+
+    until i == arr.length - 1       # cs = 2
+        # add to current sum the next value
+        current_sum += arr[j]       # cs 
+        
+        # compare and replace large_num if current_sum is greater
+        if large_sum < current_sum
+            large_sum = current_sum
+        end
+
+        # increment j to iterate through to the next value otherwise, increment i to move onto the next sum iteration
+        if j != arr.length - 1
+            j += 1
+        else
+            i += 1
+            current_sum = arr[i]
+            j = i + 1
+        end
+    end
+
+    large_sum
+end 
+
+list2 = [2, 3, -6, 7, -6, 7]
+p lcs2(list)
