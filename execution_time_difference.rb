89ed1d84a3374a1251f5965618194408e978c9ed
asiddiki98 <azimsardines@gmail.com>
2020-11-20 14:59:44 -0500

    ETD/Algorithms

--- execution_time_difference.rb
+++ execution_time_difference.rb
@@ -1,20 +1,44 @@
 # Given a list of integers find the smallest number in the list.
 require 'byebug'
-def my_min(arr)         
-    min = 0
+def my_min(arr)         #O(n^2) 
+    min = 0 #O(1)
 
-    i = 0
-    while i < arr.length - 1            # i = 0
-        j = i + 1                       # j = 2
-        while j < arr.length            
-            min = (arr[i] < min) ? arr[i] : min           # arr[i] = 0 & arr[j] = 5 & min = 0
-            j += 1
-        end
-        i += 1
-    end
-
+    (0...arr.length).each do |i|     #O(n)
+        min = arr[i] < min ? arr[i] : min    #O(1)
+        (i+1...arr.length).each do |j|    #O(n)
+            min = arr[j] < min ? arr[j] : min #O(1)
+        end 
+    end 
     min
 end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)  # =>  -5
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)  # =>  -5
+
+def my_mintwo(arr)  #O(n)
+    min = 0
+
+    i = 0
+    while i < arr.length - 1   
+        min = (arr[i] < min) ? arr[i] : min       
+        i += 1
+    end
+    min
+end
+
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_mintwo(list)  # =>  -5
+
+def lcs(array) #O(n^3)
+    subs = []
+    (0...array.length).each do |i|
+        (i+1...array.length).each do |j|
+            subs << array[i..j]
+        end
+    end
+
+    subs.map(&:sum).max
+end 
+
+list = [2, 3, -6, 7, -6, 7]
+p lcs(list)
